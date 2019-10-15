2162ec0a88825f4df2c04270559fa41e1a364a22
cgonzaru <cristina.glez.rubio@gmail.com>
2019-10-15 17:37:49 +0200

    add list of pokemons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      :D
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemons: [],
+      userInput: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getPokemons();
+  }
+
+  getPokemons() {
+    fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        pokemons: data.results
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header"><h1>Mi lista de pokemons</h1></header>
+        <main className="app__main">
+          <input type="text" className="search"/>
+          <ul className="pokemons__list">
+            {this.state.pokemons
+              .map(item => {
+                return (
+                  <li className="pokemon__card">
+                    {item.name}
+                  </li>
+                );
+              })}
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
