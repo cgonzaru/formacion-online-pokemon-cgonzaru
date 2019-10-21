3301c870f4718e6cbe85fe9838a399bf8bb355a6
cgonzaru <cristina.glez.rubio@gmail.com>
2019-10-21 12:05:03 +0200

    add docs

---
+++ docs/precache-manifest.6b9c9b9582314bff92101edcba7594bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5002ea1ee7202474cf17e7ce0d07487",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03db37ff11ca53ad1e0b",
+    "url": "./static/css/main.240495af.chunk.css"
+  },
+  {
+    "revision": "1f3e725cd75daf29e419",
+    "url": "./static/js/2.c74acb00.chunk.js"
+  },
+  {
+    "revision": "03db37ff11ca53ad1e0b",
+    "url": "./static/js/main.a8ad055d.chunk.js"
+  },
+  {
+    "revision": "1fc96437318b28c7d1fe",
+    "url": "./static/js/runtime-main.e1616e43.js"
+  }
+]);
